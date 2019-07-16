f01b03775b90e77b414eb097fb0c21a9747d2ab3
manoflogan <manoflogan@gmail.com>
2019-07-15 23:01:50 -0700

    Removing extraneous files.

--- show_open_food_trucks.py
+++ show_open_food_trucks.py
@@ -1,12 +1,17 @@
-"""Make sure to install requests before running."""
+"""Entry point to show the food resources."""
 
 
 import datetime
 import heapq
-import model
+import logging
 import os
 import requests
 import typing
+
+from logging import config
+
+config.fileConfig('logging.conf')
+logger = logging.getLogger('root')
 
 
 URL = 'https://data.sfgov.org/resource/jjew-r69b.json'
@@ -25,13 +30,13 @@ def get_mobile_foods() -> typing.List['model.Location']:
     time_string: str = '{hour}:{minute}'.format(hour=time_tuple.tm_hour,
                                                 minute=time_tuple.tm_min)
 
-    heap_list: typing.List['model.Location'] = list()
     while True:
         r = None
         params: dict = {'$limit': 10, '$offset': offset,
                         '$where': ('start24 <= "{}" AND end24 > '
                                    '"{}"'.format(time_string, time_string)),
                         'dayofweekstr': '{day}'.format(day=day_of_week)}
+
         if app_token:
             r = requests.get(URL, headers={'X-App-Token': app_token},
                              params=params)
@@ -41,21 +46,36 @@ def get_mobile_foods() -> typing.List['model.Location']:
             break
 
         response = r.json()
+        heap_list: typing.List[typing.Tuple(str, str)] = list()
         for resp in response:
-            heapq.heappush(
-                heap_list, model.Location(name=resp.get('applicant', ''),
-                                          location=resp.get('location', '')))
+            heapq.heappush(heap_list, (resp.get('applicant', ''),
+                                       resp.get('location', '')))
+
+        display(sorted(heap_list))
         offset += 10
         if len(response) < 10:
             break
         continue_command = input(
-            'Would you like to continue? Enter Y to continue, and N to abort? ')
-        if continue_command != 'Y':
+            'Would you like to continue? Enter Y to continue, and N to abort :')
+        if continue_command != 'Y' or continue_command == 'N':
             break
-    return heap_list
+
+
+def display(locations: typing.List['model.Location']):
+    print('Name      Location')
+    for loc in locations:
+        print('{name}    {location}'.format(name=loc[0], location=loc[1]))
+
+
+def display_locations():
+    heap_list = get_mobile_foods()
+    if not heap_list:
+        return
+    display(heap_list)
+    print('#####')
+    for sorted_location in sorted(heap_list):
+        print(sorted_location)
 
 
 if __name__ == '__main__':
-    heap_list = get_mobile_foods()
-    for heap in heap_list:
-        print(heap)
+    display_locations()
